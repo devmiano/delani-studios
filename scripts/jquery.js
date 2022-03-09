6ad5973e8f104baac18c1f38a5dc206b14b5466b
Miano <dev.miano@gmail.com>
2022-03-09 03:11:28 +0300

    added on click toggle to the activity section cards

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -5,6 +5,21 @@ $('#icon').on('click', function () {
   $('#img').toggle('slow');
 });
 
+// Toggle multiple elements
+
 $('.icon-text').on('click', function () {
   $('.text').toggle('slow');
+});
+
+// activity animation
+$('.card1').on('click', function () {
+  $('.content1').toggle('fast');
+});
+
+$('.card2').on('click', function () {
+  $('.content2').toggle('fast');
+});
+
+$('.card3').on('click', function () {
+  $('.content3').toggle('fast');
 });
