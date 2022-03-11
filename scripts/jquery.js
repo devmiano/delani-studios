daaa86ac737972982bfeb2df9bfd5fdc4a2c392b
Miano <dev.miano@gmail.com>
2022-03-11 22:42:07 +0300

    added jquery animations to the portfolio section, made changes to the font heaidngs in index.html

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,25 +1,82 @@
 import $ from 'jquery';
 
-// Toggle
-$('#icon').on('click', function () {
-  $('#img').toggle('slow');
-});
+$(function () {
+  // activity animation for the activity cards
+  $('.card1').on('click', function () {
+    $('.content1').toggle('fast');
+  });
 
-// Toggle multiple elements
+  $('.card2').on('click', function () {
+    $('.content2').toggle('fast');
+  });
 
-$('.icon-text').on('click', function () {
-  $('.text').toggle('slow');
-});
+  $('.card3').on('click', function () {
+    $('.content3').toggle('fast');
+  });
 
-// activity animation for the activity cards
-$('.card1').on('click', function () {
-  $('.content1').toggle('fast');
-});
+  // activity animation for the portfolio cards
 
-$('.card2').on('click', function () {
-  $('.content2').toggle('fast');
-});
+  $('#work1')
+    .on('mouseenter', function () {
+      $('#title1').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title1').hide('fast');
+    });
 
-$('.card3').on('click', function () {
-  $('.content3').toggle('fast');
+  $('#work2')
+    .on('mouseenter', function () {
+      $('#title2').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title2').hide('fast');
+    });
+
+  $('#work3')
+    .on('mouseenter', function () {
+      $('#title3').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title3').hide('fast');
+    });
+
+  $('#work4')
+    .on('mouseenter', function () {
+      $('#title4').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title4').hide('fast');
+    });
+
+  $('#work5')
+    .on('mouseenter', function () {
+      $('#title5').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title5').hide('fast');
+    });
+
+  $('#work6')
+    .on('mouseenter', function () {
+      $('#title6').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title6').hide('fast');
+    });
+
+  $('#work7')
+    .on('mouseenter', function () {
+      $('#title7').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title7').hide('fast');
+    });
+
+  $('#work8')
+    .on('mouseenter', function () {
+      $('#title8').show('fast');
+    })
+    .on('mouseleave', function () {
+      $('#title8').hide('fast');
+    });
 });
