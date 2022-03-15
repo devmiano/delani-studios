c4f20507eb97d2dfd5c65a15c843f68cf8014589
Miano <dev.miano@gmail.com>
2022-03-15 10:26:31 +0300

    changed the alert message

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 import { formReset } from './reset';
 
 var send = document.getElementById('send');
+var output = document.getElementById('output');
 
 send.onclick = function () {
   var addName = document.getElementById('name').value;
-  alert('We got your message ' + addName + ', Thank you for your feedback.');
+  output.innerHTML =
+    'We got your message ' + addName + ', Thank you for your feedback.';
 
   formReset();
 };
